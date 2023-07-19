1365d11e53612b9c796b8513430258f0330f614a
Samuelfjr <samucaftww@gmail.com>
2023-07-19 10:34:43 -0300

    style: Formata

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 function toggleMode() {
     const html = document.documentElement
-
     html.classList.toggle("light");
 
     const image = document.querySelector("#profile img");
-
     const alt = document.querySelector("#profile img");
 
     if(html.classList.contains('light')) {
         image.setAttribute('src', '../assets/profile-light.png');
-
         alt.setAttribute('alt', 'Foto do Samuel sorrindo, com barba, camiseta branca e óculos do meme "turn down for what".');    
     } else {
         image.setAttribute('src', '../assets/profile.png');
-
         alt.setAttribute('alt', 'Foto do Samuel sorrindo, com barba e camiseta branca.');
     }
 }
